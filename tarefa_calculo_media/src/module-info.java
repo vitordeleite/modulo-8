7bce94b7e55c7fdc69e91ad7c74f7e06787027c9
vitordeleite <voliveralt92@gmail.com>
2024-01-10 12:05:29 -0300

    tarefa media

---
+++ tarefa_calculo_media/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module tarefa_calculo_media {
+}
---
+++ tarefa_calculo_media/src/tarefa_calculo_media/Medias.java
@@ -0,0 +1,17 @@
+package tarefa_calculo_media;
+
+public class Medias {
+	
+	
+	public static void main(String args[]) {
+	 float nota1 = 20.0f;
+	 float nota2 = 30.0f;
+	 float nota3 = 21.0f;
+	 float nota4 = 52.0f;
+	 float media = 0.0f;
+	 media = (nota1 +nota2 +nota3 + nota4)/4;
+		
+	System.out.println(media);
+	}
+
+}
